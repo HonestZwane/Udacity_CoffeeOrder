bd4663b8a680134e736d2943dd5832c156ed76be
Ntobeko Zwane <ntobeko96zwane@gmail.com>
2018-12-15 00:57:12 +0200

    Stopped on video 188

--- MainActivity.java
+++ MainActivity.java
@@ -15,32 +15,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void SubmitOrder(View view)
+    public void SubmitOrder(View view)//calaculates order price/total...takes quantity * 5
     {
         DisplayPrice(qntty *5);
     }
-    private void Display(int number)
+    private void Display(int number)//displays number of ordered items..increses or decreases
     {
-        TextView qntty= findViewById(R.id.txtQuantity);
-        qntty.setText(number);
+        TextView qntity= findViewById(R.id.txtQuantity);
+        qntity.setText(number);//should put the ""+ or else it wont run
     }
-    private void DisplayPrice(int num)
+    private void DisplayPrice(int num)//displays total price
     {
-        TextView txtprice= findViewById(R.id.txtPrice);
-        String txt = qntty + " cups cost R" + num;
-        txtprice.setText(txt);
+        TextView txtOrderSummary= findViewById(R.id.txt_OrderSummary);
+        //String txt = qntty + " cups cost R" + num;//from part 1
+        String txt2 = createOrderSummary();
+        txtOrderSummary.setText(txt2);
         //txtprice.setText("Total " + NumberFormat.getCurrencyInstance().format(num));
     }
     public void Increase(View view)
     {
-        //int qntty = 3;
         qntty = qntty + 1;
         Display(qntty);
     }
     public void Decrease(View view)
     {
-        //int qntty = 3;
         qntty = qntty - 1;
         Display(qntty);
+    }
+    public int calculatePrice()
+    {
+        return qntty * 5;
+    }
+    public String createOrderSummary()
+    {
+        //orderPrice=SubmitOrder();
+        return "Name: Ntobeko Zwane"
+                + "\nQuantity: "+qntty
+                + "\nTotal: "+calculatePrice()
+                + "\nThank You";
     }
 }
