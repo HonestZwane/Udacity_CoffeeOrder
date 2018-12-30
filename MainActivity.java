85a1cfe8e3378095d8e08d78c7524b7281b16e1a
Ntobeko Zwane <ntobeko96zwane@gmail.com>
2018-12-30 22:27:59 +0200

    Done with it all and can send on email

--- MainActivity.java
+++ MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.ntobekozwane.myapplication321;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -14,7 +18,8 @@ public class MainActivity extends AppCompatActivity {
     int qntty = 0;
     //CheckBox whippdCream = findViewById(R.id.chkbx_whippedCream);
     boolean hasWhippedCream = false;
-    boolean hasChocolate = false;
+    boolean hasChocolate ;//= false;
+    String userName = "";
 
 
     @Override
@@ -24,18 +29,30 @@ public class MainActivity extends AppCompatActivity {
     }
     public void SubmitOrder(View view)//calaculates order price/total...takes quantity * 5
     {
+        EditText name = findViewById(R.id.edt_name);
+        userName=name.getText().toString();
+
         CheckBox whippdCream = findViewById(R.id.chkbx_whippedCream);
         hasWhippedCream= whippdCream.isChecked();
 
         CheckBox chocolate = findViewById(R.id.chkbx_chocolate);
         hasChocolate= chocolate.isChecked();
 
+        /*Intent intent =new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Coffee Order for " + userName);
+        intent.putExtra(Intent.EXTRA_TEXT, createOrderSummary());
+        if(intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);*/
+
+
         DisplayPrice(qntty *5);
     }
+
     private void Display(int number)//displays number of ordered items..increses or decreases
     {
         TextView qntity= findViewById(R.id.txtQuantity);
-        qntity.setText(number);//should put the ""+ or else it wont run
+        qntity.setText(""+number);//should put the ""+ or else it wont run
     }
     private void DisplayPrice(int num)//displays total price
     {
@@ -43,31 +60,59 @@ public class MainActivity extends AppCompatActivity {
         txtOrderSummary.setText(createOrderSummary());
 
         //Log.v("MainActivity", "Has WhippedCream " + hasWhippedCream);
-
     }
     public void Increase(View view)
     {
-        qntty = qntty + 1;
-        Display(qntty);
+        if(qntty<100)
+            qntty = qntty + 1;
+
+        else
+             Toast.makeText(this, "cant order more than 100 cups", Toast.LENGTH_SHORT).show();
+
+            Display(qntty);
     }
     public void Decrease(View view)
     {
-        qntty = qntty - 1;
+        if(qntty>0)
+            qntty = qntty - 1;
+
+        else
+            Toast.makeText(this, "cant order negative cups", Toast.LENGTH_SHORT).show();
+
         Display(qntty);
     }
     public int calculatePrice()
     {
-        return qntty * 5;
+        int basicprice=5;
+        if(hasChocolate)
+            basicprice=basicprice+1;
+
+        if(hasWhippedCream)
+            basicprice=basicprice+2;
+        //sum = qntty*5;
+
+        return basicprice * qntty;
     }
     public String createOrderSummary()
     {
         //orderPrice=SubmitOrder();
-        return "Name: Ntobeko Zwane"
+        return "Name: "+userName
                 + "\nQuantity: "+qntty
                 + "\nHas WhippedCream: " +hasWhippedCream
                 + "\nHas Chocolate: " +hasChocolate
 
                 + "\nTotal: "+calculatePrice()
                 + "\nThank You";
+    }
+
+    public void SendEmail(View view)
+    {
+        Intent intent =new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Coffee Order for " + userName);
+        intent.putExtra(Intent.EXTRA_TEXT, createOrderSummary());
+        if(intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
+
     }
 }
